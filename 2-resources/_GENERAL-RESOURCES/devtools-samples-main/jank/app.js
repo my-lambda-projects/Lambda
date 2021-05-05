2f88282d914f6ee192d44e908f5e74922843928b
bgoonz <bryan.guner@gmail.com>
2021-05-05 05:12:49 -0400

    update

--- 2-resources/_GENERAL-RESOURCES/devtools-samples-main/jank/app.js
+++ 2-resources/_GENERAL-RESOURCES/devtools-samples-main/jank/app.js
@@ -1,171 +1,320 @@
-/* Copyright 2016 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. */
-
-(function(window) {
+( function ( window ) {
   'use strict';
 
   var app = {},
-      proto = document.querySelector('.proto'),
-      movers,
-      bodySize = document.body.getBoundingClientRect(),
-      ballSize = proto.getBoundingClientRect(),
-      maxHeight = Math.floor(bodySize.height - ballSize.height),
-      maxWidth = 97, // 100vw - width of square (3vw)
-      incrementor = 10,
-      distance = 3,
-      frame,
-      minimum = 10,
-      subtract = document.querySelector('.subtract'),
-      add = document.querySelector('.add');
+    proto = document.querySelector( '.proto' ),
+    movers,
+    bodySize = document.body.getBoundingClientRect(),
+    ballSize = proto.getBoundingClientRect(),
+    maxHeight = Math.floor( bodySize.height - ballSize.height ),
+    maxWidth = 97, // 100vw - width of square (3vw)
+    incrementor = 10,
+    distance = 3,
+    frame,
+    minimum = 10,
+    subtract = document.querySelector( '.subtract' ),
+    add = document.querySelector( '.add' );
 
   app.optimize = false;
   app.count = minimum;
   app.enableApp = true;
 
   app.init = function () {
-    if (movers) {
+    if ( movers ) {
       bodySize = document.body.getBoundingClientRect();
-      for (var i = 0; i < movers.length; i++) {
-        document.body.removeChild(movers[i]);
+      for ( var i = 0; i < movers.length; i++ ) {
+        document.body.removeChild( movers[ i ] );
       }
-      document.body.appendChild(proto);
+      document.body.appendChild( proto );
       ballSize = proto.getBoundingClientRect();
-      document.body.removeChild(proto);
-      maxHeight = Math.floor(bodySize.height - ballSize.height);
+      document.body.removeChild( proto );
+      maxHeight = Math.floor( bodySize.height - ballSize.height );
     }
-    for (var i = 0; i < app.count; i++) {
+    for ( var i = 0; i < app.count; i++ ) {
       var m = proto.cloneNode();
-      var top = Math.floor(Math.random() * (maxHeight));
-      if (top === maxHeight) {
-        m.classList.add('up');
+      var top = Math.floor( Math.random() * ( maxHeight ) );
+      if ( top === maxHeight ) {
+        m.classList.add( 'up' );
       } else {
-        m.classList.add('down');
+        m.classList.add( 'down' );
       }
-      m.style.left = (i / (app.count / maxWidth)) + 'vw';
+      m.style.left = ( i / ( app.count / maxWidth ) ) + 'vw';
       m.style.top = top + 'px';
-      document.body.appendChild(m);
+      document.body.appendChild( m );
     }
-    movers = document.querySelectorAll('.mover');
+    movers = document.querySelectorAll( '.mover' );
   };
 
-  app.update = function (timestamp) {
-    for (var i = 0; i < app.count; i++) {
-      var m = movers[i];
-      if (!app.optimize) {
-        var pos = m.classList.contains('down') ?
-            m.offsetTop + distance : m.offsetTop - distance;
-        if (pos < 0) pos = 0;
-        if (pos > maxHeight) pos = maxHeight;
+  app.update = function ( timestamp ) {
+    for ( var i = 0; i < app.count; i++ ) {
+      var m = movers[ i ];
+      if ( !app.optimize ) {
+        var pos = m.classList.contains( 'down' ) ?
+          m.offsetTop + distance : m.offsetTop - distance;
+        if ( pos < 0 ) pos = 0;
+        if ( pos > maxHeight ) pos = maxHeight;
         m.style.top = pos + 'px';
-        if (m.offsetTop === 0) {
-          m.classList.remove('up');
-          m.classList.add('down');
+        if ( m.offsetTop === 0 ) {
+          m.classList.remove( 'up' );
+          m.classList.add( 'down' );
         }
-        if (m.offsetTop === maxHeight) {
-          m.classList.remove('down');
-          m.classList.add('up');
+        if ( m.offsetTop === maxHeight ) {
+          m.classList.remove( 'down' );
+          m.classList.add( 'up' );
         }
       } else {
-        var pos = parseInt(m.style.top.slice(0, m.style.top.indexOf('px')));
-        m.classList.contains('down') ? pos += distance : pos -= distance;
-        if (pos < 0) pos = 0;
-        if (pos > maxHeight) pos = maxHeight;
+        var pos = parseInt( m.style.top.slice( 0, m.style.top.indexOf( 'px' ) ) );
+        m.classList.contains( 'down' ) ? pos += distance : pos -= distance;
+        if ( pos < 0 ) pos = 0;
+        if ( pos > maxHeight ) pos = maxHeight;
         m.style.top = pos + 'px';
-        if (pos === 0) {
-          m.classList.remove('up');
-          m.classList.add('down');
+        if ( pos === 0 ) {
+          m.classList.remove( 'up' );
+          m.classList.add( 'down' );
         }
-        if (pos === maxHeight) {
-          m.classList.remove('down');
-          m.classList.add('up');
+        if ( pos === maxHeight ) {
+          m.classList.remove( 'down' );
+          m.classList.add( 'up' );
         }
       }
     }
-    frame = window.requestAnimationFrame(app.update);
+    frame = window.requestAnimationFrame( app.update );
   }
 
-  document.querySelector('.stop').addEventListener('click', function (e) {
-    if (app.enableApp) {
-      cancelAnimationFrame(frame);
+  document.querySelector( '.stop' ).addEventListener( 'click', function ( e ) {
+    if ( app.enableApp ) {
+      cancelAnimationFrame( frame );
       e.target.textContent = 'Start';
       app.enableApp = false;
     } else {
-      frame = window.requestAnimationFrame(app.update);
+      frame = window.requestAnimationFrame( app.update );
       e.target.textContent = 'Stop';
       app.enableApp = true;
     }
-  });
+  } );
 
-  document.querySelector('.optimize').addEventListener('click', function (e) {
-    if (e.target.textContent === 'Optimize') {
+  document.querySelector( '.optimize' ).addEventListener( 'click', function ( e ) {
+    if ( e.target.textContent === 'Optimize' ) {
       app.optimize = true;
       e.target.textContent = 'Un-Optimize';
     } else {
       app.optimize = false;
       e.target.textContent = 'Optimize';
     }
-  });
+  } );
 
-  add.addEventListener('click', function (e) {
-    cancelAnimationFrame(frame);
+  add.addEventListener( 'click', function ( e ) {
+    cancelAnimationFrame( frame );
     app.count += incrementor;
     subtract.disabled = false;
     app.init();
-    frame = requestAnimationFrame(app.update);
-  });
+    frame = requestAnimationFrame( app.update );
+  } );
 
-  subtract.addEventListener('click', function () {
-    cancelAnimationFrame(frame);
+  subtract.addEventListener( 'click', function () {
+    cancelAnimationFrame( frame );
     app.count -= incrementor;
     app.init();
-    frame = requestAnimationFrame(app.update);
-    if (app.count === minimum) {
+    frame = requestAnimationFrame( app.update );
+    if ( app.count === minimum ) {
       subtract.disabled = true;
     }
-  });
+  } );
 
-  function debounce(func, wait, immediate) {
+  function debounce( func, wait, immediate ) {
     var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
+    return function () {
+      var context = this,
+        args = arguments;
+      var later = function () {
         timeout = null;
-        if (!immediate) func.apply(context, args);
+        if ( !immediate ) func.apply( context, args );
       };
       var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+      clearTimeout( timeout );
+      timeout = setTimeout( later, wait );
+      if ( callNow ) func.apply( context, args );
     };
   };
 
-  var onResize = debounce(function () {
-    if (app.enableApp) {
-        cancelAnimationFrame(frame);
-        app.init();
-        frame = requestAnimationFrame(app.update);
+  var onResize = debounce( function () {
+    if ( app.enableApp ) {
+      cancelAnimationFrame( frame );
+      app.init();
+      frame = requestAnimationFrame( app.update );
     }
-  }, 500);
+  }, 500 );
 
-  window.addEventListener('resize', onResize);
+  window.addEventListener( 'resize', onResize );
 
   add.textContent = 'Add ' + incrementor;
   subtract.textContent = 'Subtract ' + incrementor;
-  document.body.removeChild(proto);
-  proto.classList.remove('.proto');
+  document.body.removeChild( proto );
+  proto.classList.remove( '.proto' );
   app.init();
   window.app = app;
-  frame = window.requestAnimationFrame(app.update);
+  frame = window.requestAnimationFrame( app.update );
 
-})(window);
+} )( window );
+
+
+window => {
+  "use strict";
+
+  let app = {},
+    proto = document.querySelector( '.proto' ),
+    movers,
+    bodySize = document.body.getBoundingClientRect(),
+    ballSize = proto.getBoundingClientRect(),
+    maxHeight = Math.floor( bodySize.height - ballSize.height ),
+    // 100vw - width of square (3vw)
+    maxWidth = 97,
+    incrementor = 10,
+    distance = 3,
+    frame,
+    minimum = 10,
+    subtract = document.querySelector( '.subtract' ),
+    add = document.querySelector( '.add' );
+
+  app.optimize = false;
+  app.count = minimum;
+  app.enableApp = true;
+
+  app.init = function () {
+    if ( movers ) {
+      bodySize = document.body.getBoundingClientRect();
+      for ( let i = 0; i < movers.length; i++ ) {
+        document.body.removeChild( movers[ i ] );
+      }
+      document.body.appendChild( proto );
+      ballSize = proto.getBoundingClientRect();
+      document.body.removeChild( proto );
+      maxHeight = Math.floor( bodySize.height - ballSize.height );
+    }
+    for ( let i = 0; i < app.count; i++ ) {
+      const m = proto.cloneNode();
+      let top = Math.floor( Math.random() * ( maxHeight ) );
+      if ( top === maxHeight ) {
+        m.classList.add( 'up' );
+      } else {
+        m.classList.add( 'down' );
+      }
+      m.style.left = `${ i / ( app.count / maxWidth ) }vw`;
+      m.style.top = `${ top }px`;
+      document.body.appendChild( m );
+    }
+    movers = document.querySelectorAll( '.mover' );
+  };
+
+  app.update = function ( timestamp ) {
+    for ( let i = 0; i < app.count; i++ ) {
+      const m = movers[ i ];
+      if ( !app.optimize ) {
+        let pos = m.classList.contains( 'down' ) ?
+          m.offsetTop + distance : m.offsetTop - distance;
+        if ( pos < 0 ) pos = 0;
+        if ( pos > maxHeight ) pos = maxHeight;
+        m.style.top = `${ pos }px`;
+        if ( m.offsetTop === 0 ) {
+          m.classList.remove( 'up' );
+          m.classList.add( 'down' );
+        }
+        if ( m.offsetTop === maxHeight ) {
+          m.classList.remove( 'down' );
+          m.classList.add( 'up' );
+        }
+      } else {
+        let pos = parseInt( m.style.top.slice( 0, m.style.top.indexOf( 'px' ) ) );
+        ( m.classList.contains( 'down' ) ? pos += distance : pos -= distance );
+        if ( pos < 0 ) pos = 0;
+        if ( pos > maxHeight ) pos = maxHeight;
+        m.style.top = `${ pos }px`;
+        if ( pos === 0 ) {
+          m.classList.remove( 'up' );
+          m.classList.add( 'down' );
+        }
+        if ( pos === maxHeight ) {
+          m.classList.remove( 'down' );
+          m.classList.add( 'up' );
+        }
+      }
+    }
+    frame = window.requestAnimationFrame( app.update );
+  }
+
+  document.querySelector( '.stop' ).addEventListener( 'click', e => {
+    if ( app.enableApp ) {
+      cancelAnimationFrame( frame );
+      e.target.textContent = 'Start';
+      app.enableApp = false;
+    } else {
+      frame = window.requestAnimationFrame( app.update );
+      e.target.textContent = 'Stop';
+      app.enableApp = true;
+    }
+  } );
+
+  document.querySelector( '.optimize' ).addEventListener( 'click', e => {
+    if ( e.target.textContent === 'Optimize' ) {
+      app.optimize = true;
+      e.target.textContent = 'Un-Optimize';
+    } else {
+      app.optimize = false;
+      e.target.textContent = 'Optimize';
+    }
+  } );
+
+  add.addEventListener( 'click', e => {
+    cancelAnimationFrame( frame );
+    app.count += incrementor;
+    subtract.disabled = false;
+    app.init();
+    frame = requestAnimationFrame( app.update );
+  } );
+
+  subtract.addEventListener( 'click', () => {
+    cancelAnimationFrame( frame );
+    app.count -= incrementor;
+    app.init();
+    frame = requestAnimationFrame( app.update );
+    if ( app.count === minimum ) {
+      subtract.disabled = true;
+    }
+  } );
+
+  function debounce( func, wait, immediate ) {
+    let timeout;
+    return function () {
+      const context = this,
+        args = arguments;
+      const later = function () {
+        timeout = null;
+        if ( !immediate ) func.apply( context, args );
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout( timeout );
+      timeout = setTimeout( later, wait );
+      if ( callNow ) func.apply( context, args );
+    };
+  };
+
+  const onResize = debounce( () => {
+    if ( app.enableApp ) {
+      cancelAnimationFrame( frame );
+      app.init();
+      frame = requestAnimationFrame( app.update );
+    }
+  }, 500 );
+
+  window.addEventListener( 'resize', onResize );
+
+  add.textContent = `Add ${ incrementor }`;
+  subtract.textContent = `Subtract ${ incrementor }`;
+  document.body.removeChild( proto );
+  proto.classList.remove( '.proto' );
+  app.init();
+  window.app = app;
+  frame = window.requestAnimationFrame( app.update );
+
+};
+( window );
