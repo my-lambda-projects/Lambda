97582c64f6ed1274396d3e5372057e6dea55309e
bgoonz <bryan.guner@gmail.com>
2021-05-24 19:17:26 -0400

    hi

--- js/bryan.js
+++ js/bryan.js
@@ -1,11 +1,19 @@
 class BryanGuner extends SoftwareEngineer {
-  constructor ( project ) {
-    this.skills = [ 'javascript', 'react', 'redux', 'express', 'node', 'python', 'sql' ]
+  constructor (project) {
+    this.skills = [
+      'javascript',
+      'react',
+      'redux',
+      'express',
+      'node',
+      'python',
+      'sql',
+    ];
     this.project = project;
   }
   workHard () {
-    while ( today() !== deadline() ) {
-      this.skills.forEach( skill => this.project.push( writeCode( skill ) ) )
+    while (today () !== deadline ()) {
+      this.skills.forEach (skill => this.project.push (writeCode (skill)));
     }
   }
 }
